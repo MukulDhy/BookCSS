a58a63a71c4fc90ef1bebd3904ce9465b37c586c
Mukul Dahiya <1029mukul38@gmail.com>
2023-03-04 15:39:46 +0530

    commit Code

---
+++ script2.js
@@ -0,0 +1,8 @@
+let background = document.querySelector(".background");
+let div2 = document.querySelectorAll(
+  ".background .row:nth-child(even) div:nth-child(2)"
+);
+let div1 = document.querySelectorAll(
+  ".background .row:nth-child(odd) div:nth-child(1)"
+);
+let icon = document.querySelectorAll(".background .row div i");
